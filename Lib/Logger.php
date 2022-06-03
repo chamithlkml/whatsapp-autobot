5032444764d665b7cdfa50a80e5ffe4b525ed85a
Chamith Jayaweera <chamithlkml@gmail.com>
2022-06-03 11:53:58 +0530

    Add comments to Logger class

--- Lib/Logger.php
+++ Lib/Logger.php
@@ -5,17 +5,23 @@ class Logger
 {
     private static $log_path = './api.log';
 
+    /**
+     * Log info message
+     * @param $message
+     */
     public static function info($message){
         self::log($message, 'INFO');
     }
 
+    /**
+     * Log error message
+     * @param $message
+     */
     public static function error($message){
         self::log($message, 'ERROR');
     }
 
     private static function log($message, $type){
-//        $log_model = new LogModel();
-//        $log_model->insert_log($type, $message);
         $timely_message = date('Y-m-d H:i:s') . ' ' . $type . ' ' . $message . "\n";
         file_put_contents(self::$log_path, $timely_message, FILE_APPEND);
     }
