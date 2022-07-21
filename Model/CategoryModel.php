7b3dd22fcacbd5cd70eca5115369db233f02876c
hansinihirunika <hansinihiru2017@gmail.com>
2022-07-21 12:03:37 +0530

    Removed comment lines.

--- Model/CategoryModel.php
+++ Model/CategoryModel.php
@@ -7,7 +7,6 @@ class CategoryModel extends Database{
     public function insert_category($category_name, $description, $type){
         $stmt = $this->connection->prepare("INSERT INTO whatsapp_item_categories (category_name, description,type) VALUES (?, ?, ?)");
             
-            // print_r( $row);
             $stmt->bind_param("sss",$category_name,$description,$type);
 
             $result = $stmt->execute();
@@ -17,7 +16,7 @@ class CategoryModel extends Database{
         $categories= $this->select("SELECT id,category_name FROM whatsapp_item_categories");
         
         if(count($categories) == 0){
-            throw new Exception('Existing order not found');
+            throw new Exception('Category not found');
         }
 
         return $categories;
@@ -34,29 +33,4 @@ class CategoryModel extends Database{
         
     }
 
-
-    // public function insert_categories($sheetData){
-    //     // print_r($sheetData); 
-    //    if ($sheetData===null) {
-    //     return;
-    //    }
-    //     foreach ($sheetData as $row) {
-        
-    //         $stmt = $this->connection->prepare("INSERT INTO whatsapp_item_categories (category_name, description,type) VALUES (?, ?, ?)");
-    //         $category_name=$row[0];
-    //         $description=$row[1]?$row[1]:"";
-    //         $type=(int)$row[2];
-    //         // print_r( $row);
-    //         $stmt->bind_param("sss",$category_name,$description,$type);
-
-                
-    //         $result = $stmt->execute();
-    //     }
-
-    // }
-
-    // public function truncate_categories(){
-    //     $stmt = $this->connection->prepare("TRUNCATE TABLE whatsapp_item_categories");
-    //     $result = $stmt->execute();
-    // }
 }
--- import_category.php
+++ import_category.php
@@ -1,18 +1,5 @@
 <?php
 
-/**
- * 1. Truncate the 2 tables before start
- * 2. Rename inc/.config.php to inc/config.php ************
- * 3. Replace database credentials **********
- * 4. require_once /path/to/inc/bootstrap.php *******
- * 5. Use Model/OrderModel.php file's existing methods
- * 6. AddCategory($name, $description)
- * 7. addMenuItem($category_id, $item_name, $description, $size, $currency, $price, $type)
- * 8. print_r($var); die;
- * 9. echo 'test...';
- * 10. repair Lib/composer.json and `run php composer.phar`
- */
-
 require_once 'inc/bootstrap.php';
 require_once 'Model/CategoryModel.php';
 require_once 'Model/MenuItemModel.php';
@@ -32,7 +19,6 @@ try{
 
     unset($sheetData[0]);
 
-    // print_r($sheetData); 
     foreach($sheetData as $row){
         $category_name=$row[0];
         $description=$row[1]?$row[1]:"";
@@ -43,7 +29,7 @@ try{
     
 }catch(Exception $e){
     echo "Exception thrown: " . $e->getMessage();
-    // echo "Trace: " . $e->
+    
 }
 
 ?>
